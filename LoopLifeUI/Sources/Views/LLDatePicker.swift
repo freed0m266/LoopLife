33d76a71070ce5a685b44d077664a9e6c6ca650a
Martin Svoboda <martin.svoboda@eucdigitalis.cz>
2025-12-26 22:50:55 +0100

    💫 Add animations for LLDatePicker

--- LoopLifeUI/Sources/Views/LLDatePicker.swift
+++ LoopLifeUI/Sources/Views/LLDatePicker.swift
@@ -39,6 +39,8 @@ public struct LLDatePicker: View {
 			)
 			.titleLarge()
 			.foregroundColor(textColor)
+			.animation(.default, value: text)
+			.contentTransition(.numericText())
 			.padding(.vertical, 12)
 			.padding(.horizontal, 16)
 			.background {
